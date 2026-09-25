a3b8bf59bf079af86b610378fcbe1f66d102bedb
sitabja biswas <sitabjabiswas633@gmail.com>
2026-09-25 20:20:49 +0530

    production fix

--- Backend/server.js
+++ Backend/server.js
@@ -23,18 +23,20 @@ app.use('/api/orders', require('./routes/orderRoutes'));
 // app.use('/api/payment', require('./routes/paymentRoutes'));
 app.use('/api/analytics', require('./routes/analyticsRoutes'));
 
-// // Serve frontend in production
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../frontend/build')));
-  
-//   app.use((req, res) => {
-//     res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
-//   });
-// } else {
+// Serve frontend in production
+if (process.env.NODE_ENV === 'production') {
+  const frontendBuildPath = path.join(__dirname, '../frontend/build');
+
+  app.use(express.static(frontendBuildPath));
+
+  app.get(/^(?!\/api).*/, (req, res) => {
+    res.sendFile(path.resolve(frontendBuildPath, 'index.html'));
+  });
+} else {
   app.get('/', (req, res) => {
     res.send('ShopNest API is running in Development mode...');
   });
-// }
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
